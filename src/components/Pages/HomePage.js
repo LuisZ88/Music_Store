a0fac07c74646e0332a7cac5141a60d2a8b4edcb
Luis <acosta88co@gmail.com>
2022-03-29 18:58:36 +0200

    heroku

--- src/components/Pages/HomePage.js
+++ src/components/Pages/HomePage.js
@@ -13,7 +13,7 @@ return(
   <Image className='' fluid style={{ maxHeight: "100vh", objectFit: "contain" }} src='https://res.cloudinary.com/pruebasmern/image/upload/v1648540503/Music%20Shop/Front_page_nemctc.png' onClick={goTo}/>
   </div>
   <div style={{ maxHeight: "100vh" }}className='d-flex justify-content-center d-md-none'>
-  <Image className='' fluid style={{ maxHeight: "100vh", objectFit: "contain" }} src='https://res.cloudinary.com/pruebasmern/image/upload/v1648548774/wallsm_awzcg8.png' onClick={goTo}/>
+  <Image className='' fluid style={{ maxHeight: "100vh", objectFit: "contain" }} src='https://res.cloudinary.com/pruebasmern/image/upload/v1648573081/Black_Guitar_Illustration_Phone_Wallpaper_iwl8mw.png' onClick={goTo}/>
   </div></>)}
 
 
