2f0cd8dcc9fe704128cc3417a80b585c7ba596b6
Luis <acosta88co@gmail.com>
2022-04-04 13:12:22 +0200

    1.00

--- src/components/Nav/Footer.js
+++ src/components/Nav/Footer.js
@@ -5,7 +5,7 @@ return (    <footer className="w-100 py-4 flex-shrink-0 shadow">
     <div className="row gy-4 gx-5">
         <div className="col-lg-4 col-md-6">
             <h5 className="h1 text-white">Musicalia.</h5>
-            <p className="small text-muted">Web de pruebas OnePage desarrollada con React</p>
+            <p className="small text-muted">Web de pruebas Single Page desarrollada con React</p>
             <p className="small text-muted mb-0">&copy; Copyrights. All rights reserved. </p>
         </div>
       
